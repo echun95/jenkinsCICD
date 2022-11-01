d1853e406e12f5201061f3a91973a3bd73a89120
leeyoungchun <dldydcns123@gmail.com>
2022-11-01 22:49:26 +0900

    gitignore 생성

--- src/main/java/com/njonecompany/web/WebInitializer.java
+++ src/main/java/com/njonecompany/web/WebInitializer.java
@@ -5,6 +5,7 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 
 public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
+
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return null;
